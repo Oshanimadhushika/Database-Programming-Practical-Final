573e8eedd25ca2f138b8e3c69f3a564debf85252
oshani madhushika <oshanimadhushika12@gmail.com>
2022-07-12 11:28:28 +0530

    Implements method to StudentDAOIMpl from CrudDAO And StudentDAO

--- GDSE_Programme/src/ijse/lk/dbp/dao/custom/StudentDAO.java
+++ GDSE_Programme/src/ijse/lk/dbp/dao/custom/StudentDAO.java
@@ -1,4 +1,12 @@
 package ijse.lk.dbp.dao.custom;
 
-public interface StudentDAO {
+import ijse.lk.dbp.dao.CrudDAO;
+import ijse.lk.dbp.entity.Student;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface StudentDAO extends CrudDAO<Student,String> {
+    public ArrayList<Student> searchStudents(String enteredText) throws SQLException, ClassNotFoundException;
+
 }
--- GDSE_Programme/src/ijse/lk/dbp/dao/custom/impl/StudentDAOImpl.java
+++ GDSE_Programme/src/ijse/lk/dbp/dao/custom/impl/StudentDAOImpl.java
@@ -1,4 +1,44 @@
 package ijse.lk.dbp.dao.custom.impl;
 
-public class StudentDAOImpl {
+import ijse.lk.dbp.dao.custom.StudentDAO;
+import ijse.lk.dbp.entity.Student;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class StudentDAOImpl implements StudentDAO {
+    @Override
+    public ArrayList<Student> searchStudents(String enteredText) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Student> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(Student dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Student dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Student search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
 }

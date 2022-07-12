a293a190a4353af354ce2267fa9ad7c12884d16a
oshani madhushika <oshanimadhushika12@gmail.com>
2022-07-12 11:07:59 +0530

    Create model for Student

--- GDSE_Programme/src/ijse/lk/dbp/model/Student.java
+++ GDSE_Programme/src/ijse/lk/dbp/model/Student.java
@@ -1,4 +1,18 @@
 package ijse.lk.dbp.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Student {
+    String student_id;
+    String student_name;
+    String email;
+    String contact;
+    String address;
+    String nic;
 }
+

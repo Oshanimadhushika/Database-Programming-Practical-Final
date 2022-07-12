666add42917e74632cc13ee9ba86da5c3a635256
oshani madhushika <oshanimadhushika12@gmail.com>
2022-07-12 11:05:04 +0530

    Create TM for Student

--- GDSE_Programme/src/ijse/lk/dbp/view/tdm/StudentTM.java
+++ GDSE_Programme/src/ijse/lk/dbp/view/tdm/StudentTM.java
@@ -1,4 +1,17 @@
 package ijse.lk.dbp.view.tdm;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class StudentTM {
+    String student_id;
+    String student_name;
+    String email;
+    String contact;
+    String address;
+    String nic;
 }

e90796a7a83d942ab88390b6bed1b51227e39602
oshani madhushika <oshanimadhushika12@gmail.com>
2022-07-12 11:33:11 +0530

    Create DAOFactory for hide Object Creation logic

--- GDSE_Programme/src/ijse/lk/dbp/dao/DAOFactory.java
+++ GDSE_Programme/src/ijse/lk/dbp/dao/DAOFactory.java
@@ -1,4 +1,33 @@
 package ijse.lk.dbp.dao;
 
+import ijse.lk.dbp.dao.custom.impl.StudentDAOImpl;
+
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+
+
+    public static DAOFactory getDaoFactory() {
+        if (daoFactory == null) {
+            daoFactory = new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+    public enum DAOTypes {
+        STUDENT
+    }
+
+    public SuperDAO getDAO(DAOTypes types) {
+        switch (types) {
+            case STUDENT:
+                return new StudentDAOImpl();
+            default:
+                return null;
+        }
+    }
+
+
 }
